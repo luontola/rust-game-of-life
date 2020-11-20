4fe3fc9a701c2bdcd16befffa167a7dc444248db
Esko Luontola <esko@luontola.fi>
2020-11-20 12:18:44 +0200

    Cell struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(PartialEq, Debug)]
+struct Cell {
+    x: i32,
+    y: i32,
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_cell_equality() {
+        let cell1 = Cell { x: 1, y: 2 };
+        let cell2 = Cell { x: 1, y: 2 };
+        assert_eq!(cell1, cell2);
+    }
+}
